40ab5bdda052f4eb94bfb14fb5bb543383da7649
Shuai <sudapink@gmail.com>
2016-07-21 22:07:47 -0400

    remove cnpj

--- getAverage.R
+++ getAverage.R
@@ -1,7 +1,19 @@
-library(openxlsx)
+require(openxlsx)
 fileName <-"2011 employee.xlsx"
-dataFrame <- read.xlsx2(fileName,sheetIndex=1,header=T,colClasses=NA)
-dataFrame<-as.numeric(dataFrame)
-dataFrame$Leader<-rowMeans(subset(dataFrame,
+dataFrame <- read.xlsx(fileName,colNames = T)
+dataFrame[dataFrame=="N"]<-NA
+dat <- as.data.frame(sapply(dataFrame, as.numeric)) #<- sapply is here
+dat$Leader<-rowMeans(subset(dat,
 select=c("Q2","Q13","Q14","Q19","Q20","Q24","Q29","Q40","Q47","Q61")),
 na.rm=T)
+dat$peer<-rowMeans(subset(dat,
+                            select=c("Q5","Q11","Q36","Q39","Q51","Q55","Q58")),
+                     na.rm=T)
+dat$stake<-rowMeans(subset(dat,
+                          select=c("Q1","Q7","Q22","Q23","Q25","Q31","Q33","Q43")),
+                   na.rm=T)
+dat$motive<-rowMeans(subset(dat,
+                           select=c("Q6","Q16","Q21","Q60")),
+                    na.rm=T)
+dat$climate<-rowMeans(subset(dat,select=c("Leader","peer","stake","motive")))
+newData<-subset(dat,select=c("Leader","peer","stake","motive","climate"))
